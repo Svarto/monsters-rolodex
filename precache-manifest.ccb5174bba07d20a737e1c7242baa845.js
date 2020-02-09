37897849d46739d165693d51befe58dfa2610e3c
Svarto <svarto@protonmail.com>
2020-02-09 21:53:51 +0100

    Updates

---
+++ precache-manifest.ccb5174bba07d20a737e1c7242baa845.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "826274ecae8aa2670b70512db2d51db3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cff3432d42089f37655a",
+    "url": "/monsters-rolodex/static/css/main.aa4a5224.chunk.css"
+  },
+  {
+    "revision": "01fa59518f48c0c9b0f6",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.650d6c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cff3432d42089f37655a",
+    "url": "/monsters-rolodex/static/js/main.cf703f03.chunk.js"
+  },
+  {
+    "revision": "420e781ce2f2f4d61faa",
+    "url": "/monsters-rolodex/static/js/runtime-main.9cf5dfda.js"
+  }
+]);
