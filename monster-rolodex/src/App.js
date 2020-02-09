d54a6f4d55262c7210ab9e1ebf2e33fddfcdd170
Svarto <svarto@protonmail.com>
2020-02-09 12:35:16 +0100

    React components

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component.jsx'
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string: 'Hello Philip'
+      monsters: [],
+      searchField: ''
     };
   }
-  
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => {
+      this.setState({ monsters: users })})
+  }
+
   render() {
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({ string: 'Hello Hanna'})}>Change Text</button>
-        </header>
+      <input type='search' placeholder='search monsters' onChange={e => this.setState( { searchField: e.target.value } )}/>
+      <CardList monsters={this.state.monsters} />>
       </div>
     );
   }
